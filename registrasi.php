6ec943d4ad52665ea2163eb30e02d1a23313becf
aripirwansyah <aripirwnsyh69@gmail.com>
2023-08-31 17:09:26 +0700

    new

---
+++ registrasi.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Registrasi</title>
+</head>
+
+<body>
+    <h1>Halaman Registrasi</h1>
+
+    <form action="" method="post">
+        <li>
+            <label for="username">username: </label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="passsword">passsword: </label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <label for="passsword2">konfirmasi passsword: </label>
+            <input type="password2" name="password2" id="password2">
+        </li>
+        <li>
+            <button></button>
+        </li>
+    </form>
+</body>
+</html>
